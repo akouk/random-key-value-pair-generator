2aaf004b5b5b49cc31ff44d0897f096f8d0ec209
akouk <77749065+akouk@users.noreply.github.com>
2023-01-07 20:18:44 +0200

    Update random_data_creation.py

--- random_data_creation.py
+++ random_data_creation.py
@@ -8,7 +8,8 @@ from typing import Type, Union, Dict
 # chance to add [] value in key
 EMPTY_DATA_CHANCE = 20
 
-class ValueGenerator():
+class ValueGenerator:
+
     # Function that generates random string value (combination of letters and number) up to the specified length by -l parameter 
     def generate_random_string(self, max_string_length: int) -> str:
         return ''.join(random.choices(string.ascii_letters + string.digits, k = max_string_length))
@@ -35,148 +36,149 @@ class ValueGenerator():
             return self.generate_random_float(0.0,100.0)
 
 
-class KeyValuePairGenerator:
+class KeyValuePairGenerator(ValueGenerator):
       
-    def __init__(self, key_file_dict: dict, value_generator: ValueGenerator):
-        self.key_file_dict = key_file_dict
-        self.value_generator = value_generator
+    def __init__(self, kv_dict: dict) -> None:
+        self.kv_dict = kv_dict
     
-    # is_root checks if recursion is in root object. If so, the value can be None and the key must be unique   
+    @staticmethod
     def generate_random_KeyValue_pair(
-        self, 
         num_lines: int, 
         max_nesting: int, 
-        max_keys: int, 
-        max_string_length: int, 
-        keyFile_dict: dict, 
+        max_string_length: int,
+        max_keys: int,
+        kv_dict: dict,
         is_root: bool
-        ) -> Dict[str, Union[str, int, float, None, Dict]]:
+    ) -> Dict[str, Union[str, int, float, None, Dict]]:
 
         random_data = {}
 
         # Generate a random number of keys up to the maximum specified by the -m parameter
         num_keys = random.randint(1, max_keys)
-        increasing_num = str(0)
+
+        value_generator = ValueGenerator()
 
         for i in range(num_keys):
-            key = random.choice(list(keyFile_dict.keys()))
-            data_type = self.keyFile_dict[key]
+            key = random.choice(list(kv_dict.keys()))
+            data_type = kv_dict[key]
 
             if is_root:
-                key += increasing_num
+                key += value_generator.generate_random_string(max_string_length)
 
             # If the maximum level of nesting is 0, then its at the bottom level and random values can be generated for each key, up to the specified keys inside each value
-            random_data[key] = self.value_generator.generate_random_value(data_type, max_string_length)
+            random_data[key] = value_generator.generate_random_value(data_type, max_string_length, is_root)
 
             # If the maximum level of nesting is greater than 0, then a random number of keys for the current level of nesting can be generated, up to the specified keys inside each value
             # and the function can be called recursively, to generate the values of each these keys
-            if max_nesting > 0 and random_data[key] != None :
-                random_data[key] = self.generate_random_KeyValue_pair(
+            if max_nesting > 0 and random_data[key] != None:
+                random_data[key] = KeyValuePairGenerator.generate_random_KeyValue_pair(
                     num_lines, 
                     max_nesting-1, 
-                    max_keys, 
                     max_string_length, 
-                    keyFile_dict, 
-                    False)
-
-            increasing_num += str(1)
-
+                    max_keys,
+                    kv_dict,
+                    False
+                )
+                
         return random_data
 
 
-class DataGenerator():
-    def __init__(self, key_value_pair_generator: KeyValuePairGenerator):
-        self.key_value_pair_generator = key_value_pair_generator
+class DataGenerator(KeyValuePairGenerator):
+    def __init__(
+        self, 
+        input_key_file: str,
+        num_lines: int,
+        max_nesting: int,
+        max_string_length: int,
+        max_keys: int,
+        file_to_generate_data: str
+    ) -> None:
 
-    # Function that reads the given keyFile.txt and stores each lines key and its data type in a dictionary
-    def read_keyFile_and_store_keys_and_their_data_types(self, keyFile: str) -> dict:
-        keyFile_dict = {}
+        self.input_key_file = input_key_file
+        self.num_lines = num_lines
+        self.max_nesting = max_nesting
+        self.max_string_length = max_string_length
+        self.max_keys = max_keys
+        self.file_to_gerate_data = file_to_generate_data
 
-        # Open the file and read its lines
-        with open(keyFile, "r") as keyFile:
-            lines = keyFile.readlines()
+        # Function that reads the given .txt file and stores each lines key and its data type in a dictionary
+    def read_key_file(self, input_file: str) -> dict:
+        key_dict = {}
 
-            # Read each line of the file and store the key and its data type in the dictionary
-            for each_line in lines:
+        key_file = open(input_file, "r")
+        file_lines = key_file.readlines()
+        key_file.close()
+        for each_line in file_lines:
+            try:
+                key_name, data_type = each_line.strip().split()
+                key_dict[key_name] = data_type
+            except SyntaxError:
+                print("Invalid Syntax. Each line must contain two parts: key data type")
 
-                # Check if the line contains two parts (key and data type)
-                if len(each_line.strip().split(" ")) == 2:
-                    key_name, data_type = each_line.strip().split()
-                    keyFile_dict[key_name] = data_type
-                else:
-                    # Skip the current iteration of the loop if the line does not contain two parts and ignore that line
-                    continue
-            
         # Return the dictionary
-        return keyFile_dict
+        return key_dict
 
     # Function that generates and writes the data to a file
-    def generate_and_write_data_to_file(
-        self, 
-        num_lines: int, 
-        max_nesting: int, 
-        max_keys: int, 
-        max_string_length: int, 
-        keyFile: str
-        ):
+    def generate_and_write_data_to_file(self) -> None:
 
         # Read the key file and store the keys and their data types in a dictionary
-        keyFile_dict = self.read_keyFile_and_store_keys_and_their_data_types(keyFile)
-        
-        # If keyFile dictionary is empty, then return
-        if not keyFile_dict:
-            return
+        self.kv_dict = self.read_key_file(self.input_key_file)
 
-        with open("dataToIndex2.txt", "w") as generated_file: # Open the output file for writing
+        with open(self.file_to_gerate_data, "w") as generated_file: # Open the output file for writing
 
-            for i in range(num_lines):
+            for i in range(self.num_lines):
                 # Generate a random key-value pair
-                generated_data = self.key_value_pair_generator.generate_random_KeyValue_pair(
-                    num_lines, 
-                    max_nesting, 
-                    max_keys, 
-                    max_string_length, 
-                    keyFile_dict, 
+                generated_data = KeyValuePairGenerator.generate_random_KeyValue_pair(
+                    self.num_lines, 
+                    self.max_nesting,
+                    self.max_string_length,
+                    self.max_keys,
+                    self.kv_dict,
                     True
-                    )
+                )
                 generated_file.write(json.dumps(generated_data) + "\n") # Write the data to the output file in JSON format
 
         generated_file.close() # Close the output file
 
 
 def parse_dataCreation_arguments():
-
     dataCreation_parser = argparse.ArgumentParser(description="Data Creation Parser")
     dataCreation_parser.add_argument(
         "-k", 
         type=txt_file, 
         required=True, 
         help="File containing a space-separated list of key names and their data types"
-        )
+    )
     dataCreation_parser.add_argument(
         "-n", 
         type=int_type, 
         required=True, 
         help="Number of lines (i.e. separate data) to generate"
-        )
+    )
     dataCreation_parser.add_argument(
         "-d", 
         type=int_type, 
         required=True, 
         help="Maximum level of nesting"
-        )
+    )
     dataCreation_parser.add_argument(
         "-l", 
         type=int_type, 
         required=True, 
         help="Maximum length of a string value"
-        )
+    )
     dataCreation_parser.add_argument(
         "-m", 
         type=int_type, 
         required=True, 
         help="Maximum number of keys inside each value"
-        )
+    )
+    dataCreation_parser.add_argument(
+        "-o",
+        type=str,
+        required=True,
+        help="Name of the file to write the data"
+    )
     dataCreation_args = dataCreation_parser.parse_args()
     return dataCreation_args
 
@@ -198,14 +200,18 @@ def int_type(arg):
 
 def main():
     dataCreation_args = parse_dataCreation_arguments()
-    DataGenerator.generate_and_write_data_to_file(
+    generate_data = DataGenerator(
+        dataCreation_args.k, 
         dataCreation_args.n, 
         dataCreation_args.d, 
-        dataCreation_args.m, 
         dataCreation_args.l, 
-        dataCreation_args.k
-        )
+        dataCreation_args.m,
+        dataCreation_args.o
+    )
+    generate_data.generate_and_write_data_to_file()
 
 
 if __name__ == "__main__":
     main()
+
+# python3 random_data_generation.py -k keyFile.txt -n 50 -d 3 -l 4 -m 5 -o outputFile.txt
